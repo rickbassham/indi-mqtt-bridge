d290ff434af048a63b06bc33c9653bedc8faf8d9
Robert Bradley <robbrad182@gmail.com>
2023-09-25 12:48:35 +0100

    feat: Adding MQTT username and password and improved logging (#2)

--- src/index.js
+++ src/index.js
@@ -1,94 +1,118 @@
 const mqtt = require("mqtt");
+const { INDIClient, getProperties, enableBLOB, mapping, newSwitchVector, newTextVector, newNumberVector } = require("indi-client");
+const winston = require("winston");
+const config = require("./config"); // Import configuration
 
-const {
-  getProperties,
-  enableBLOB,
-  mapping,
-  newSwitchVector,
-  newTextVector,
-  newNumberVector,
-  INDIClient,
-} = require("indi-client");
+// Configure the Winston logger
+const logger = winston.createLogger({
+  level: config.LOG_LEVEL, // Set the log level as needed
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.printf(({ timestamp, level, message }) => {
+      return `${timestamp} [${level}] ${message}`;
+    })
+  ),
+  transports: [new winston.transports.Console()], // You can add more transports if needed
+});
 
-const brokerURL = process.env.MQTT_BROKER_URL || "mqtt://127.0.0.1";
-const indiHost = process.env.INDI_HOST || "127.0.0.1";
-const indiPort = process.env.INDI_PORT ? parseInt(process.env.INDI_PORT) : 7624;
+const mqttOptions = {
+  username: config.MQTT_USERNAME,
+  password: config.MQTT_PASSWORD,
+};
+
+const brokerURL = config.MQTT_BROKER_URL;
+const indiHost = config.INDI_HOST;
+const indiPort = config.INDI_PORT;
+const baseMqttTopic = config.MQTT_BASE_TOPIC;
 
 const indiClient = new INDIClient(indiHost, indiPort);
 
 indiClient.on("connect", () => {
-  console.log("indi connection connected");
-
+  logger.info("INDI connection connected");
   indiClient.getProperties();
   indiClient.enableBLOB(null, null, "Also"); // Get blobs as well as everything else.
-})
+});
 
 indiClient.on("close", () => {
-  console.log("indi connection closed");
-})
+  logger.info("INDI connection closed");
+});
 
 indiClient.connect();
 
-const mqttClient = mqtt.connect(brokerURL);
+const mqttClient = mqtt.connect(brokerURL, mqttOptions);
 
-Object.keys(mapping).forEach(key => {
+Object.keys(mapping).forEach((key) => {
   indiClient.on(key, (obj) => {
-    let topic = `indi/data/${obj.device}`;
+    let topic = `${baseMqttTopic}/${obj.device}`;
 
     if (obj.name) topic += `/${obj.name}`;
 
     topic += `/${key}`;
 
     mqttClient.publish(topic, JSON.stringify(obj));
-  })
-});
-
-mqttClient.on("connect", () => {
-  mqttClient.subscribe("indi/commands/+", (err) => {
-    if (err)
-      console.error(err);
+    logger.debug(`Published MQTT message on topic: ${topic}`);
   });
 });
 
-mqttClient.on("message", (topic, message) => {
-  if (topic.endsWith("newTextVector")) {
-    const obj = newTextVector.fromJSON(JSON.parse(message));
-    indiClient.send(obj);
-  } else if (topic.endsWith("newSwitchVector")) {
-    const obj = newSwitchVector.fromJSON(JSON.parse(message));
-    indiClient.send(obj);
-  } else if (topic.endsWith("newNumberVector")) {
-    const obj = newNumberVector.fromJSON(JSON.parse(message));
-    indiClient.send(obj);
-  } else if (topic.endsWith("getProperties")) {
-    const obj = getProperties.fromJSON(JSON.parse(message));
-    indiClient.send(obj);
-  } else if (topic.endsWith("enableBLOB")) {
-    const obj = enableBLOB.fromJSON(JSON.parse(message));
-    indiClient.send(obj);
-  }
+mqttClient.on("connect", () => {
+  mqttClient.subscribe(`${baseMqttTopic}/+/+`, (err) => {
+    if (err) {
+      logger.error(`Error subscribing to MQTT topic: ${err.message}`);
+    } else {
+      logger.info("Connected to MQTT broker");
+    }
+  });
 });
 
-// The signals we want to handle
-// NOTE: although it is tempting, the SIGKILL signal (9) cannot be intercepted and handled
-var signals = {
-  'SIGHUP': 1,
-  'SIGINT': 2,
-  'SIGTERM': 15
+const handleMqttMessage = (topic, message) => {
+  const topicSuffix = topic.split("/").pop();
+  let obj;
+
+  switch (topicSuffix) {
+    case "newTextVector":
+      obj = newTextVector.fromJSON(JSON.parse(message));
+      break;
+    case "newSwitchVector":
+      obj = newSwitchVector.fromJSON(JSON.parse(message));
+      break;
+    case "newNumberVector":
+      obj = newNumberVector.fromJSON(JSON.parse(message));
+      break;
+    case "getProperties":
+      obj = getProperties.fromJSON(JSON.parse(message));
+      break;
+    case "enableBLOB":
+      obj = enableBLOB.fromJSON(JSON.parse(message));
+      break;
+    default:
+      break;
+  }
+
+  if (obj) {
+    indiClient.send(obj);
+    logger.debug(`Received and processed MQTT message on topic: ${topic}`);
+  }
 };
 
-// Do any necessary shutdown logic for our application here
-const shutdown = (signal, value) => {
-  console.log("shutdown!");
+mqttClient.on("message", handleMqttMessage);
 
+// Handle shutdown signals
+const signals = {
+  SIGHUP: 1,
+  SIGINT: 2,
+  SIGTERM: 15,
+};
+
+const shutdown = (signal, value) => {
+  logger.info("Shutdown signal received. Shutting down...");
   indiClient.close();
   mqttClient.end();
 };
 
-// Create a listener for each of the signals that we want to handle
 Object.keys(signals).forEach((signal) => {
   process.on(signal, () => {
-    console.log(`process received a ${signal} signal`);
+    logger.info(`Process received a ${signal} signal`);
     shutdown(signal, signals[signal]);
   });
 });
+
